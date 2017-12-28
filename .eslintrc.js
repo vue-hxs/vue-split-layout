ea1ddafdc867b3b8dcfd218f5a5459f52cbdbfeb
luis <stdiopt@gmail.com>
2017-12-28 01:15:39 -0200

    added overflow hidden to split

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: [ 'standard', 'plugin:vue/recommended']
+  extends: ['standard', 'plugin:vue/recommended']
 }
--- demo/App.vue
+++ demo/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div :class="['app', state.extraStyle?'extra':'']">
     <div class="controls">
-      <button @click="toggleEdit" :class="{active:state.edit}" >Toggle editable <span></span></button>
-      <button @click="toggleResize" :class="{active:state.resize}">Toggle resizeable <span></span></button>
+      <button @click="toggleEdit" :class="{active:state.edit}" >Toggle editable <span/></button>
+      <button @click="toggleResize" :class="{active:state.resize}">Toggle resizeable <span/></button>
       <button @click="toggleBoth" :class="{active: state.edit && state.resize}">Toggle both</button>
       <button @click="toggleStyle" :class="{active:state.extraStyle}">toggle Style</button>
     </div>
 
     <Layout :edit="state.edit" :resize="state.resize" :splits="state.splits">
-      <div class="nopane"><div>Also drag me on the gray area<button>random button </button><ul><li>Random</li><li>list</li></ul><div>{{state.edit}}</div></div></div>
+      <div class="nopane">
+        <div>Also drag me on the gray area<button>random button </button><ul><li>Random</li><li>list</li></ul><div>{{ state.edit }}</div></div>
+      </div>
       <Pane title="Drag me">testing</Pane>
       <Pane title="Drag me too">Stuff<MyInput /></Pane>
     </Layout>
